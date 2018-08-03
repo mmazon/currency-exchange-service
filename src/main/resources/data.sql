ccc5afad4cd0e4afbddf4d19baac0cec5ae3a4d3
Moacir Mazon <moacir.mazon@gmail.com>
2018-08-03 14:20:39 -0300

    Add h2 e jpa

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into exchange_value (id, currency_from, currency_to, conversion_multiple,port)
+values(10001, 'USD', 'BRL', 3.7,0);
+insert into exchange_value (id, currency_from, currency_to, conversion_multiple,port)
+values(10002, 'EUR', 'BRL', 4.5,0);
+insert into exchange_value (id, currency_from, currency_to, conversion_multiple,port)
+values(10003, 'AUD', 'BRL', 4.2,0);
