862efd0579c31db176a37d2ce132584d94a9409d
Moacir Mazon <moacir.mazon@gmail.com>
2018-08-03 15:27:04 -0300

    Add repository methods

--- src/main/java/com/mazon/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/mazon/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,7 +1,5 @@
 package com.mazon.microservices.currencyexchangeservice;
 
-import java.math.BigDecimal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,9 +12,12 @@ public class CurrencyExchangeController {
 	@Autowired
 	private Environment enviroment;
 	
+	@Autowired
+	private ExchangeValueRepository repo;
+	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public ExchangeValue retreiveExchangeValue(@PathVariable String from, @PathVariable String to){
-		ExchangeValue exchangeValue = new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+		ExchangeValue exchangeValue = repo.findByFromAndTo(from, to);
 		exchangeValue.setPort(Integer.parseInt(enviroment.getProperty("local.server.port")));
 		return exchangeValue;
 	}
